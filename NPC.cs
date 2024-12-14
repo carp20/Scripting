aab2fa3e7102273407e6d26c9ad82616dfd0b681
carp20 <rijiymisha@gmail.com>
2024-12-14 16:34:55 +0300

    NPC
    NPC info

---
+++ NPC.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int health = 5;
+    public int level = 1;
+    public float speed = 1.2f;
+    public string name = "name";
+    public int damage = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
